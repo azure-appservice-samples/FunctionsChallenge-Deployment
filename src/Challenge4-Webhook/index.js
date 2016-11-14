fb159f3acae0d70d7c012932fbb0c9eca4ce0ebf
Cory Fowler <cory@syntaxc4.net>
2016-11-14 14:58:11 -0800

    Update index.js

--- src/Challenge4-Webhook/index.js
+++ src/Challenge4-Webhook/index.js
@@ -2,7 +2,7 @@ module.exports = function (context, data) {
     context.log('Webhook was triggered!');
 
     context.res = {
-      SubscriptionId: process.env.SubscriptionId
+      SubscriptionId: process.env.ChallengeKey
     };
     
     context.done();
